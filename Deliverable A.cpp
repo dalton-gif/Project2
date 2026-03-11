30850dc930b08460abbb577feaf3e623b14c3cea
AaryaKanpile <aaryakanpile@tamu.edu>
2026-03-11 16:29:38 -0500

    Added User Queries for Tet Id and Vertex Id

--- Deliverable A.cpp
+++ Deliverable A.cpp
@@ -12,7 +12,7 @@
 
 const double epsV = 1e-10; // Vertex Tolerance used for quantisation
 
-// Store vertices in SoA form //
+// Stores vertices as parallel arrays using SoA form //
 
 std::vector<double> Vx;
 std::vector<double> Vy;
@@ -21,16 +21,16 @@ std::vector<double> Vz;
 //Each tetrahedron is 4 vertex IDs
 
 struct Tet{
-    int v[4];  // indices for the tetrahedron vertices
+    int v[4];  // Stores indices (vertex IDs) for one tetrahedron 
 };
 
-//All tets
+//Stores all tets as an array
 std::vector<Tet> T;
 
-//Quantised coordinates structure (qx, qy, qz)
+//Key creation structure: Quantised coordinates structure (qx, qy, qz)
 typedef std::tuple<long long, long long, long long> Key;
 
-// Function to add the quantised vertices to a Hash table
+// Data structure to add the quantised vertices to a Hash table
 struct KeyHash {
     std::size_t operator() (const Key& k) const {
         
@@ -46,7 +46,7 @@ struct KeyHash {
     }
 };
 
-// Function to check equality of vertex coordinates 
+// Data structure to check equality between two keys 
 
 struct KeyEq {
     bool operator()(const Key& a, const Key&b) const{
@@ -56,20 +56,21 @@ struct KeyEq {
     }
 };
 
-//Map from quantised key to vertex ID
+// Global Hash Map: for mapping quantised coordinates to vertex ID(keys)
+
 std::unordered_map<Key , int, KeyHash, KeyEq> key_to_vertex_id;
 
 // Function that creates the vertex ID
 
 int getVertexID(double x,double y, double z) {
 
-    //Quantize coordinates
+    //Quantizing coordinates
     
     long long qx = static_cast<long long>(std::llround(x/epsV));
     long long qy = static_cast<long long>(std::llround(y/epsV));
     long long qz = static_cast<long long>(std::llround(z/epsV));
 
-    Key key (qx,qy,qz); // Putting the quantised coordinates back together
+    Key key (qx,qy,qz); // Putting the quantised coordinates in a Key
 
     // To check if this key already exists
     auto it = key_to_vertex_id.find(key);
@@ -82,7 +83,7 @@ int getVertexID(double x,double y, double z) {
 
     Vx.push_back(x);
     Vy.push_back(y);
-    Vz.push_back(y);
+    Vz.push_back(z);
 
     key_to_vertex_id[key] = new_id; //Stores the key in the map is new ID
 
@@ -104,7 +105,8 @@ std:: string line; // holds one line of text at a time
 int line_number = 0; // Tracks which line you are on
 
 //Read file line by line
-while (std::getline(in,line)) {
+
+while (std::getline(in,line)) {  // Each non-empty, non-comment line read by getline
     ++line_number;
 
     //Skip empty lines
@@ -124,7 +126,8 @@ while (std::getline(in,line)) {
     // Reading the vertices in a tetrahedron
     if (!(iss >> x[0] >> y[0] >> z[0]
               >> x[1] >> y[1] >> z[1]
-              >> x[1] >> y[1] >> z[1])) {
+              >> x[2] >> y[2] >> z[2]
+              >> x[3] >> y[3] >> z[3])) {
 
                 std::cerr << "Warning: Could not parse numeric data on line"
                           << line_number << "\n";
@@ -147,21 +150,39 @@ in.close(); //closes the input file
 std:: cout << "Number of unique vertices: " <<Vx.size() << "\n";
 std:: cout << "Number of tets: " << T.size() << "\n";
 
-//Print the first tet and its vertices 
+// User Queries
 if (!T.empty()) {
-    std::cout << "First tet vertex IDs: "
-              << T[1].v[0] << " "
-              << T[1].v[1] << " "
-              << T[1].v[2] << " "
-              << T[1].v[3] << "\n";
-    
-    int id0 = T[1].v[0];
-    std::cout << "Vertex " << id0 << " coords:"
-              << Vx[id0] << " " << Vy[id0] << " " << Vz[id0] << "\n";
+    int tet_id;
+    std::cout << "Enter a tet ID (0 to " << static_cast<int>(T.size()) - 1
+              << "): ";
+    std::cin >> tet_id; // Prompts user to enter a tet ID 
+
+    if (tet_id < 0 || tet_id >= static_cast<int>(T.size())) {
+        std::cerr << "Invalid tet ID.\n"; // Invalid tet ID argument if ID is out of range
+    } else {
+        std::cout << "Tet " << tet_id << " vertex IDs: "
+                  << T[tet_id].v[0] << " "
+                  << T[tet_id].v[1] << " "
+                  << T[tet_id].v[2] << " "
+                  << T[tet_id].v[3] << "\n"; // Prints the 4 vertex IDs for that tet ID
+
+        int vid;
+        std::cout << "Enter a vertex ID (0 to"  
+                  << static_cast<int>(Vx.size()) - 1 << "): ";
+        std::cin >> vid; // Prompts the user for vertex ID
+        
+        if (vid < 0 || vid >= static_cast<int>(Vx.size())) {
+            std::cerr << "Invalid vertex ID. \n"; // Invalid vertex ID if out of range
+        } else {
+            std:: cout << "Vertex " << vid << " coords: "
+                                    << Vx[vid] << " "
+                                    << Vy[vid] << " "
+                                    << Vz[vid] << "\n"; // Prints vertex ID
+        }
+    }
 }
 
-    return 0;
-}
+return 0;}
 
  
 
