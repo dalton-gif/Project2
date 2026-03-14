d3fbf84e261ad62d7516514b1a0d640df238448b
Manohar <manohar.reddyummadi@tamu.edu>
2026-03-14 02:10:31 -0500

    Commiting and Updating small editings Done

--- mesh_export.hpp
+++ mesh_export.hpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <cstdint>
 
+/*Initilaising a Class Mesh to get Vectors of vertices and 
+  Tets with Connectivity which is linked/defined in main.cpp*/
+
 class Mesh
 {
 public:
@@ -15,6 +18,8 @@ public:
     std::vector<uint32_t> t1;
     std::vector<uint32_t> t2;
     std::vector<uint32_t> t3;
+
+    // Calling exportMSH function defined in export_mesh.cpp
 
     void exportMSH(const std::string& filename);
 };
