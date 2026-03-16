7d85b6a3d936013e0c808942a01f0b4bd9ab7827
Manohar-reddy-ummadi <manohar.reddyummadi@gmail.com>
2026-03-16 10:17:22 -0500

    Update Mesh export Part in main.cpp

--- main.cpp
+++ main.cpp
@@ -67,19 +67,17 @@ int main() {
 
     Mesh mesh;                  // Mesh is initialised in mesh_export.hpp
 
-        mesh.x = Vx;
-        mesh.y = Vy;
-        mesh.z = Vz;
+        mesh.x = Vx;            // Assigning X- coord Vector to mesh.x
+        mesh.y = Vy;            // Assigning Y- coord Vector to mesh.y
+        mesh.z = Vz;            // Assigning Z- coord Vector to mesh.z
 
     for (const Tet& tet : T)
-    {
-
-        mesh.t0.push_back(tet.v[0]);
-        mesh.t1.push_back(tet.v[1]);
-        mesh.t2.push_back(tet.v[2]);
-        mesh.t3.push_back(tet.v[3]);
-
+    {   mesh.t0.push_back(tet.v[0]);  // Add first vertex index of Tet to mesh.to 
+        mesh.t1.push_back(tet.v[1]);  // Add second vertex index of Tet to mesh.t1 
+        mesh.t2.push_back(tet.v[2]);  // Add third vertex index of Tet to mesh.t2 
+        mesh.t3.push_back(tet.v[3]);  // Add fourth vertex index of Tet to mesh.t3 
     }
+    
     // export.MSH -> defined in mesh_export.cpp
     mesh.exportMSH("New_exported_mesh.msh");
 
